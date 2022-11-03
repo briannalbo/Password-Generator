457dfa47ad5a43bfd001c9abf0c807e2f0d37870
unknown <brianna.albo@gmail.com>
2022-11-03 18:14:55 -0400

    added more if else statements to produce all outcomes for generator options

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,7 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/";
+var everything = "!@#$%^&*()_-+={}[];:'`~<,>.?/0987654321abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var passwordLength;
 var haveLowercase;
 var includeUppercase;
@@ -35,7 +36,7 @@ function determineLength(){
   return passwordLength;
 }
 
-
+//this function determines if the user would like to include lowercase characters
 function determineLowercase(){
   haveLowercase = prompt("Do you want to include lowercase letters in your password? \n(Yes or No)");
   haveLowercase = haveLowercase.toLowerCase();
@@ -108,6 +109,7 @@ function determineNumbers(){
 
 function determineSpecial(){
   haveSpecial = prompt("Do you want to include special characters? \n(Yes or No)");
+  haveSpecial = haveSpecial.toLowerCase();
   
   if (haveSpecial === null || haveSpecial === ""){
     alert("Please answer Yes or No");
@@ -144,11 +146,14 @@ function generatePassword(){
 
   var characters;
   var password = "";
-  if (haveLowercase && includeUppercase && includeNumbers){
+  if (haveLowercase && includeUppercase && includeNumbers && haveSpecial){
+    characters = lowercase + uppercase + numbers + specialChar;
+  }
+  else if (haveLowercase && includeUppercase && includeNumbers){
     characters = lowercase + uppercase + numbers;
   }
-  else if (haveLowercase && includeUppercase && includeNumbers && haveSpecial){
-    characters = lowercase + uppercase + numbers + specialChar;
+  else if (haveLowercase && includeNumbers){
+    characters = lowercase + numbers;
   }
   else if (haveLowercase && haveSpecial){
   characters = lowercase + specialChar;
@@ -156,8 +161,17 @@ function generatePassword(){
   else if (haveLowercase && includeUppercase && haveSpecial){
     characters = lowercase + uppercase + specialChar;
   }
+  else if (haveLowercase && includeNumbers && haveSpecial){
+    characters = lowercase + numbers + specialChar;
+  }
+  else if (includeUppercase && includeNumbers && haveSpecial){
+    characters = uppercase + numbers + specialChar;
+  }
+  else if (includeUppercase && haveSpecial){
+    characters = uppercase + specialChar;
+  }
   else if (includeUppercase && includeNumbers){
-    characters == uppercase + numbers;
+    characters = uppercase + numbers;
   }
   else if (includeUppercase && haveLowercase){
     characters = uppercase + lowercase;
@@ -166,18 +180,24 @@ function generatePassword(){
     characters = numbers + specialChar;
   }
   else if (includeUppercase){
-    characters == uppercase;
+    characters = uppercase;
   }
   else if (haveLowercase){
-    characters == lowercase;
+    characters = lowercase;
+  }
+  else if (includeNumbers){
+    characters = numbers;
+  }
+  else if (haveSpecial){
+    characters = specialChar;
   }
 
   else {
-     alert("error");
+     alert("You must select at least one of the following: Lowercase letters, Uppercase letters, numbers, special characters.");
   }
 
   for(var i = 0; i < passwordLength; i++){
-     characters.charAt(Math.floor(Math.random() * passwordLength.length));
+     password += characters.charAt(Math.floor(Math.random() * characters.length));
   }
 
   return password;
