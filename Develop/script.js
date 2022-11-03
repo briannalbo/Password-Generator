4fa0b562c509cafbd6c7e4c1f6df989c7e2f29be
unknown <brianna.albo@gmail.com>
2022-11-03 13:16:36 -0400

    adds function to choose whether uppercase letters will be included

--- Develop/script.js
+++ Develop/script.js
@@ -8,11 +8,12 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var characters = "!@#\$%^&*()-_=+[]{};:'<>,./?`~|";
-var uppercaseNumber;
+var includeUppercase;
 var lowercaseNumber;
 var numberNumber;
 var charactersNumber;
 
+//this function determines the length of the password. uses if statement to ensure user enters a number and that number is between 8-128.
 function determineLength(){
   passwordLength = prompt("How many characters would you like your password to have? (must be between 8-128 characters):");
 
@@ -34,12 +35,45 @@ function determineLength(){
   return passwordLength;
 }
 
+function determineUppercase(){
+  includeUppercase = prompt("Do you want to include uppercase letters in your password? \n(Yes or No)");
+  includeUppercase = includeUppercase.toLowerCase();
+
+  if (includeUppercase === "yes" || includeUppercase === "y"){
+    includeUppercase = true;
+    return includeUppercase;
+  }
+  else if (includeUppercase === "no" || includeUppercase === "n"){
+    includeUppercase = false;
+    return includeUppercase;
+}
+  else if (includeUppercase === null || includeUppercase === ""){
+    alert("This field is required.");
+    determineUppercase();
+  }
+  else {
+    alert("This field is required.");
+    determineUppercase();
+  }
+}
+
 function generatePassword(){
   determineLength();
+  determineUppercase();
 
+  var characters = lowercase;
+  var password = "";
+
+  if (includeUppercase){
+    characters += uppercase;
+  }
+
+  else{
+    characters === lowercase;
+  }
 
   for(var i = 0; i < passwordLength; i++){
-    password += characters.charAt(Math.floor(Math.random() * characters.length));
+    password += characters.charAt(Math.random() * characters.length);
   }
 
   return password;
